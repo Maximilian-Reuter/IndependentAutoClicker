bffedade74b9fd3094c85979d44e2a55f3f3f959
Maximilian-Reuter <reuterma@uni-bremen.de>
2016-12-15 05:41:40 +0100

    Add project files.

---
+++ IndependentAutoClicker/MainWindow.xaml.vb
@@ -0,0 +1,14 @@
+﻿Class MainWindow
+
+    Public Sub New()
+
+        ' This call is required by the designer.
+        InitializeComponent()
+
+        ' Add any initialization after the InitializeComponent() call.
+
+        cmb_cps.ItemsSource = Enumerable.Range(1, 100)
+
+    End Sub
+
+End Class
